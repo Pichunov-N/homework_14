e388d361e9898375ef582d505b2a714e7e9cc9a8
Pichunov-N <npichunov@gmail.com>
2022-06-12 16:29:30 +0300

    corrected some mistakes

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const getAlbums = async () => {
         const response = await fetch('https://jsonplaceholder.typicode.com/albums')
         const request = await response.json();
         const contentNode = document.querySelector('#albums');
-        const titleItem = request.map(item => item.title).forEach(element => {
-            li = document.createElement('li');
-            contentNode.append(li);
-            li.innerHTML = element;
+        request.forEach(element => {
+            let li = document.createElement('li');
+            li.innerHTML = element.title;
             li.className = 'album_title';
+            contentNode.append(li);
         });
     } catch (error) {
         console.error(error);
